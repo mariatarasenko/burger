5706ca11e6b8466327c02cdfe21f2ce3e94b4c05
Maria TArasenko <mariaatarasenko@gmail.com>
2017-11-12 21:50:13 +0300

    13th commit adaptive

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 
 function showMenu(event) {
     event.preventDefault();
-    document.querySelector('.burger_nav').style.display='block';
+    document.querySelector('.burger_nav').style.display='block'; //open the navigation menu
     
 }
        
 function hideMenu(event) {
     event.preventDefault();
    document.querySelector('.burger_nav').style.display='none';
-    
+    //close the navigation menu
 }
        
       
