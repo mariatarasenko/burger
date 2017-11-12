77b64d9b81c0b4a0067c28dd9200956690cb602f
Maria TArasenko <mariaatarasenko@gmail.com>
2017-11-12 12:32:35 +0300

    12th commit non-adaptive

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+
+function showMenu(event) {
+    event.preventDefault();
+    document.querySelector('.burger_nav').style.display='block';
+    
+}
+       
+function hideMenu(event) {
+    event.preventDefault();
+   document.querySelector('.burger_nav').style.display='none';
+    
+}
+       
+      
+
