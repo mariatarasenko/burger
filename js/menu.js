1ed64a8ba4500e610bf460e0ff7283828d61574c
Maria TArasenko <mariaatarasenko@gmail.com>
2017-11-15 16:38:46 +0300

    14th commit change js

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,22 @@
+// var openMenu = document.getElementsByClassName('burger_nav');
+// var closeMenu = document.getElementsByClassName('close_icon');
+// var button = document.getElementsByClassName('burger_menu');
 
-function showMenu(event) {
-    event.preventDefault();
-    document.querySelector('.burger_nav').style.display='block'; //open the navigation menu
-    
+// button.addEventListener('click', function(){
+//     openMenu.classList.add('burger_nav');
+// });
+
+// console.log(openMenu);
+
+
+$(document).ready()
+function changeText(){
+    var $containerWidth=$(window).width();
+    if ( $containerWidth <= 480){
+        $('.reviews_descr_butt').text('читать отзыв')
+};
+console.log($containerWidth);
 }
-       
-function hideMenu(event) {
-    event.preventDefault();
-   document.querySelector('.burger_nav').style.display='none';
-    //close the navigation menu
-}
-       
-      
+
+changeText();
 
